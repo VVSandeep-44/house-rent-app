f3544b9cef23d7b350f6435380eef0b7d17fe6ed
V VIJAY SANDEEP <vsandeep9644@gmail.com>
2026-02-15 22:04:42 +0530

    Create Property model

---
+++ backend/models/Property.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const propertySchema = new mongoose.Schema(
+  {
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    isAvailable: {
+      type: Boolean,
+      default: true,
+    },
+    description: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Property", propertySchema);
