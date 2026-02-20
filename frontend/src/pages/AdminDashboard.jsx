279bbcacb8d1da0f22954a7d55a6b3c99766744b
V VIJAY SANDEEP <vsandeep9644@gmail.com>
2026-02-20 12:14:20 +0530

    Fix admin approval route and complete admin dashboard functionality

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -1,5 +1,60 @@
+import { useEffect, useState } from "react";
+import API from "../services/api";
+
 function AdminDashboard() {
-    return <h2>Admin Dashboard</h2>;
+  const [owners, setOwners] = useState([]);
+
+
+  useEffect(() => {
+  const fetchPendingOwners = async () => {
+    try {
+      const res = await API.get("/admin/owners");
+      setOwners(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  fetchPendingOwners();
+}, []);
+
+
+  const approveOwner = async (ownerId) => {
+  try {
+    await API.put(`/admin/approve/${ownerId}`);
+
+    const res = await API.get("/admin/owners");
+    setOwners(res.data);
+
+    alert("Owner approved");
+  } catch (error) {
+    alert(error.response?.data?.message || "Approval failed");
+  }
+};
+
+
+  return (
+    <div style={{ padding: "40px" }}>
+      <h2>Admin Dashboard</h2>
+
+      {owners.map((owner) => (
+        <div
+          key={owner._id}
+          style={{
+            border: "1px solid gray",
+            margin: "10px",
+            padding: "10px",
+          }}
+        >
+          <p>Name: {owner.name}</p>
+
+          <button onClick={() => approveOwner(owner._id)}>
+            Approve
+          </button>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default AdminDashboard;
